c398533babbcad8804645c84cc6aaf20f9fc9098
金诗书 <1115346181@qq.com>
2022-06-19 14:38:58 +0800

    111

--- ODAuthenticTest/ODAuthenticTest/main.m
+++ ODAuthenticTest/ODAuthenticTest/main.m
@@ -416,6 +416,321 @@ void AT9() {
     
 }
 
+#pragma mark - 消消乐游戏
+// mMbccbc
+void AT10() {
+    char str[1000];
+    scanf("%s", str);
+    NSString *ocs = [NSString stringWithFormat:@"%s", str];
+    NSMutableArray *arr = [NSMutableArray array];
+    for (int i = 0; i < ocs.length; i++) {
+        NSString *s = [ocs substringWithRange:NSMakeRange(i, 1)];
+        if ([s isEqualToString:[arr lastObject]]) {
+            [arr removeLastObject];
+        } else {
+            [arr addObject:s];
+        }
+    }
+    printf("%lu", (unsigned long)arr.count);
+}
+
+#pragma mark - 找终点
+// 7 5 9 4 2 6 8 3 5 4 3 9 -> 2
+// 1 2 3 7 1 5 9 3 2 1 -> -1
+void AT11() {
+    char num[1000];
+    gets(num);
+    NSString *ocs = [NSString stringWithFormat:@"%s", num];
+    NSArray *arr = [ocs componentsSeparatedByString:@" "];
+    int l = (int)arr.count / 2;
+    int res = INT_MAX;
+    for (int i = 1; i < l; i++) {
+        int sum = i;
+        int cnt = 1;
+        while (sum < arr.count - 1) {
+            sum = sum + [[arr objectAtIndex:sum] intValue];
+            cnt += 1;
+        }
+        if (sum == arr.count - 1) {
+            res = MIN(cnt, res);
+        }
+    }
+    if (res == INT_MAX) {
+        printf("-1\n");
+    } else {
+        printf("%d\n", res);
+    }
+}
+
+#pragma mark - 分月饼
+// 2 4  -> 2
+int distribute(int m, int p, int k) {
+    if (p <= 0) {
+        return 0;;
+    }
+    if (m <= 0) {
+        return 0;
+    }
+    if (m == 1) {
+        if (p >= k && p <= k + 3) {
+            return 1;
+        }
+        return 0;
+    }
+    int ncount=0;
+    for (int i = k; i <= k+3; i++) {
+        ncount = ncount + distribute(m-1, p-i, i);
+    }
+    return ncount;
+}
+
+void AT12() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+    int p = n - m;
+    int count = 0;
+    
+    for(int i = 0;i < p; i++) {
+        count = count + distribute(m - 1, p - i, i);
+    }
+    printf("%d", count);
+}
+
+#pragma mark - 工号不够用了怎么办
+// 260 1
+void AT13() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    if (x <= 26) {
+        printf("1");
+        return;
+    }
+    int del = 1;
+    for (int i = 0; i < y; i++) {
+        del *= 26;
+    }
+    int r = 1;
+    del *= 10;
+    while (del < x) {
+        del *= 10;
+        r++;
+    }
+    printf("%d", r);
+}
+
+#pragma mark - 火锅
+void AT14() {
+    
+}
+
+#pragma mark - 找朋友
+//2
+//100 95 -> 0 0
+//8
+//123 124 125 121 119 122 126 123 -> 1 2 6 5 5 6 0 0
+void AT15() {
+    int n;
+    scanf("%d\n", &n);
+    char h[1000];
+    gets(h);
+    NSString *ocs = [NSString stringWithFormat:@"%s", h];
+    NSArray *hArr = [ocs componentsSeparatedByString:@" "];
+    
+    NSMutableArray *fArr = [NSMutableArray array];
+    for (int j = 0; j < hArr.count; j++) {
+        NSNumber *f = @(0);
+        for (int k = j + 1; k < hArr.count; k++) {
+            int h1 = [[hArr objectAtIndex:j] intValue];
+            int h2 = [[hArr objectAtIndex:k] intValue];
+            if (h2 > h1) {
+                f = @(k);
+                break;
+            }
+        }
+        [fArr addObject:f];
+    }
+    printf("%s\n", [fArr componentsJoinedByString:@" "].UTF8String);
+}
+
+#pragma mark - 最大花费金额
+//23,26,36,27
+//78
+void AT16() {
+    char str[10000];
+    gets(str);
+    NSString *ocs = [NSString stringWithFormat:@"%s", str];
+    NSArray *arr = [ocs componentsSeparatedByString:@","];
+    
+    int n;
+    scanf("%d", &n);
+    int max = -1;
+    for (int i = 0; i < arr.count; i++) {
+        for (int j = i + 1; j < arr.count; j++) {
+            for (int k = j + 1; k < arr.count; k++) {
+                int k1 = [[arr objectAtIndex:i] intValue];
+                int k2 = [[arr objectAtIndex:j] intValue];
+                int k3 = [[arr objectAtIndex:k] intValue];
+                int sum = k1 + k2 + k3;
+                if (sum <= n) {
+                    max = MAX(sum, max);
+                }
+            }
+        }
+    }
+    printf("%d", max);
+}
+
+#pragma mark - 最大值
+// str1是否小于str2
+BOOL compareStr(NSString *str1, NSString *str2) {
+    NSString *s1 = [str1 stringByAppendingString:str2];
+    NSString *s2 = [str2 stringByAppendingString:str1];
+    
+    return [s1 intValue] < [s2 intValue];
+    
+//    if (str1.length > str2.length) {
+//        // str1=3, str=34 -> str1=33, str2=34
+//        // 取短子串即str2的最后一位
+//        NSString *lastS = [str2 substringWithRange:NSMakeRange(str2.length - 1, 1)];
+//        NSInteger len = str1.length - str2.length;
+//        for (int i = 0; i < len; i++) {
+//            str2 = [str2 stringByAppendingString:lastS];
+//        }
+//        if ([str1 intValue] < [str2 intValue]) {
+//            return YES;
+//        }
+//        return NO;
+//    }
+//    NSString *lastS = [str1 substringWithRange:NSMakeRange(str1.length - 1, 1)];
+//    NSInteger len = str2.length - str1.length;
+//    for (int i = 0; i < len; i++) {
+//        str1 = [str1 stringByAppendingString:lastS];
+//    }
+//    if ([str1 intValue] < [str2 intValue]) {
+//        return YES;
+//    }
+//    return NO;
+}
+// 1,20,23,4,8 => 8423201
+// 3,30,34,5,9 => 9534330
+void AT17() {
+    char str[1000];
+    scanf("%s", str);
+    NSString *ocs = [NSString stringWithFormat:@"%s", str];
+    NSMutableArray *arr = [ocs componentsSeparatedByString:@","].mutableCopy;
+    for (int i = 0; i < arr.count; i++) {
+        for (int j = 0; j < arr.count - i - 1; j++) {
+            NSString *str1 = [arr objectAtIndex:j];
+            NSString *str2 = [arr objectAtIndex:j + 1];
+            if (compareStr(str1, str2)) {
+                [arr exchangeObjectAtIndex:j withObjectAtIndex:j + 1];
+            }
+        }
+    }
+    printf("%s\n", [arr componentsJoinedByString:@""].UTF8String);
+}
+
+#pragma mark - 寻找身高相近的小朋友
+//100 10
+//95 96 97 98 99 101 102 103 104 105   => 99 101 98 102 97 103 96 104 95 105
+void AT18() {
+    int h, n;
+    scanf("%d %d\n", &h, &n);
+    char str[1000];
+    gets(str);
+    NSString *ocs = [NSString stringWithFormat:@"%s", str];
+    NSArray *arr = [ocs componentsSeparatedByString:@" "];
+    NSMutableArray *hArr = [NSMutableArray array];
+    NSMutableArray *lArr = [NSMutableArray array];
+    for (int i = 0; i < arr.count; i++) {
+        int a = [[arr objectAtIndex:i] intValue];
+        int b = abs(h - a);
+        [hArr addObject:@(a)];
+        [lArr addObject:@(b)];
+    }
+    
+    for (int j = 0; j < lArr.count; j++) {
+        for (int k = 0; k < lArr.count - j - 1; k++) {
+            int aa = [[lArr objectAtIndex:k] intValue];
+            int bb = [[lArr objectAtIndex:k + 1] intValue];
+            if (aa > bb) {
+                [lArr exchangeObjectAtIndex:k withObjectAtIndex:k + 1];
+                [hArr exchangeObjectAtIndex:k withObjectAtIndex:k + 1];
+            }
+            if (aa == bb) {
+                int hh1 = [[hArr objectAtIndex:k] intValue];
+                int hh2 = [[hArr objectAtIndex:k + 1] intValue];
+                if (hh1 > hh2) {
+                    [lArr exchangeObjectAtIndex:k withObjectAtIndex:k + 1];
+                    [hArr exchangeObjectAtIndex:k withObjectAtIndex:k + 1];
+                }
+            }
+        }
+    }
+    printf("%s", [hArr componentsJoinedByString:@" "].UTF8String);
+}
+
+#pragma mark - 相对开音节
+// ekam a ekac
+// !ekam a ekekac
+void AT20() {
+    char str[1000];
+    gets(str);
+    NSString *ocs = [NSString stringWithFormat:@"%s", str];
+    NSArray *arr = [ocs componentsSeparatedByString:@" "];
+    int cnt = 0;
+    for (int i = 0; i < arr.count; i++) {
+        NSString *cs = [arr objectAtIndex:i];
+        if (cs.length < 4) {
+            continue;
+        }
+        BOOL flag = YES;
+        for (int j = 0; j < cs.length; j++) {
+            char c = [cs characterAtIndex:j];
+            if (!(c >= 'a' && c <= 'z')) {
+                flag = NO;
+                break;
+            }
+            
+        }
+        if (!flag) {
+            continue;
+        }
+        NSString *res = @"";
+        for (int j = cs.length - 1; j >= 0; j--) {
+            NSString *c = [cs substringWithRange:NSMakeRange(j, 1)];
+            res = [res stringByAppendingString:c];
+        }
+        NSString *subword = @"";
+        for (int k = 0; k < res.length; k++) {
+            if (k + 4 > res.length) {
+                break;
+            }
+            subword = [res substringWithRange:NSMakeRange(k, 4)];
+            NSString *s1 = [subword substringWithRange:NSMakeRange(0, 1)];
+            if ([@"aeiou" containsString:s1]) {
+                continue;
+            }
+            NSString *s2 = [subword substringWithRange:NSMakeRange(1, 1)];
+            if (![@"aeiou" containsString:s2]) {
+                continue;
+            }
+            NSString *s3 = [subword substringWithRange:NSMakeRange(2, 1)];
+            if ([@"aeiour" containsString:s3]) {
+                continue;
+            }
+            NSString *s4 = [subword substringWithRange:NSMakeRange(3, 1)];
+            if (![@"e" isEqualToString:s4]) {
+                continue;
+            }
+            cnt++;
+        }
+    }
+    printf("%d", cnt);
+}
+
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 //        AT1();
@@ -430,7 +745,27 @@ int main(int argc, const char * argv[]) {
         
 //        AT6();
         
-        AT9();
+//        AT9();
+        
+//        AT10();
+        
+//        AT11();
+        
+//        AT12();
+        
+//        AT13();
+        
+        AT14();
+        
+//        AT15();
+        
+//        AT16();
+        
+//        AT17();
+        
+//        AT18();
+        
+        AT20();
     }
     return 0;
 }
